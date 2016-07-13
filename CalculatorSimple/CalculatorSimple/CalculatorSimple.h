79980f595bb953ab09aa6660c8dd07d25c57f7a3
surentharbe <surentharbe@gmail.com>
2016-07-13 11:36:56 +0530

    SimpleCalculator Using C++

---
+++ CalculatorSimple/CalculatorSimple/CalculatorSimple.h
@@ -0,0 +1,32 @@
+
+// CalculatorSimple.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CCalculatorSimpleApp:
+// See CalculatorSimple.cpp for the implementation of this class
+//
+
+class CCalculatorSimpleApp : public CWinApp
+{
+public:
+	CCalculatorSimpleApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCalculatorSimpleApp theApp;
