d0893cbea494d337cc9722130bbe53d3e59bd49c
surentharbe <surentharbe@gmail.com>
2016-07-13 12:22:14 +0530

    C# Concept

---
+++ CSharp_Project/CSharp_Project/Delegates.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Delegates:
+ * C# delegates are similar to pointers to functions, in C or C++. 
+ * A delegate is a reference type variable that holds the reference to a method. 
+ * The reference can be changed at runtime.
+*/
+
+//using System;
+
+//delegate int NumberChanger(int n);
+//namespace DelegateAppl
+//{
+//    class TestDelegate
+//    {
+//        static int num = 10;
+//        public static int AddNum(int p)
+//        {
+//            num += p;
+//            return num;
+//        }
+
+//        public static int MultNum(int q)
+//        {
+//            num *= q;
+//            return num;
+//        }
+//        public static int getNum()
+//        {
+//            return num;
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            //create delegate instances
+//            NumberChanger nc1 = new NumberChanger(AddNum);
+//            NumberChanger nc2 = new NumberChanger(MultNum);
+
+//            //calling the methods using the delegate objects
+//            nc1(25);
+//            Console.WriteLine("Value of Num: {0}", getNum());
+//            nc2(5);
+//            Console.WriteLine("Value of Num: {0}", getNum());
+//            Console.ReadKey();
+//        }
+//    }
+//}
---
+++ CSharp_Project/CSharp_Project/Events.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Event:
+ * Events are user actions such as key press, clicks, mouse movements, etc., or some occurrence such as system generated notifications. 
+ * Applications need to respond to events when they occur. 
+ * For example, interrupts. Events are used for inter-process communication.
+*/
+
+using System;
+namespace SimpleEvent
+{
+    using System;
+
+    public class EventTest
+    {
+        private int value;
+        public delegate void NumManipulationHandler();
+        public event NumManipulationHandler ChangeNum;
+        protected virtual void OnNumChanged()
+        {
+            if (ChangeNum != null)
+            {
+                ChangeNum();
+            }
+            else
+            {
+                Console.WriteLine("Event fired!");
+            }
+        }
+
+        public EventTest(int n)
+        {
+            SetValue(n);
+        }
+
+        public void SetValue(int n)
+        {
+            if (value != n)
+            {
+                value = n;
+                OnNumChanged();
+            }
+        }
+    }
+
+    public class MainClass
+    {
+        public static void Main()
+        {
+            EventTest e = new EventTest(5);
+            e.SetValue(7);
+            e.SetValue(11);
+            Console.ReadKey();
+        }
+    }
+}
